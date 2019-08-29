e13e0db3c0626f4f7a31234c751ac0f0de205c89
Hugo MARQUET <humarque@e1r3p19.42.fr>
2019-08-29 04:55:13 +0200

    save

--- ex04/header.php
+++ ex04/header.php
@@ -1,11 +1,10 @@
-
 <!DOCTYPE html>
 <html>
   <head>
     <meta charset="utf-8">
     <meta name="description" content="This is an example of a meta description. This will often show up in search results.">
     <meta name=viewport content="width=device-width, initial-scale=1">
-    <title></title>
+    <title>login</title>
     <link rel="stylesheet" href="style.css">
   </head>
   <body>
--- ex04/signup.php
+++ ex04/signup.php
@@ -1,19 +1,24 @@
 <?php
-    require "header.php";
+  require "header.php";
 ?>
-<main>
-    <div class="wrapper-main">
-      <section class="section-default">  
-        <h1>Signup</h1>
-        <form action="includes/signup.inc.php" method="post">
-            <input type="text" name="uid" placeholder="Username">
-            <input type="text" name="mail" placeholder="E-Mail">
-            <input type="password" name="pwd" placeholder="Password">
-            <input type="password" name="pwdrepeat" placeholder="Repeat password">
-            <button type="submit" name="signup-submit">Signup</button>
-    </div>
-</main>
+<head>
+    <link rel="stylesheet" src="style.cs">
+</head>
+    <main>
+      <div class="wrapper-main">
+        <section class="section-default">
+          <h1>Signup</h1>
+          <form class="signupp" action="includes/signup.inc.php" method="post">
+            <input  class ="forminput" type="text" name="uid" placeholder="Username">
+            <input class ="forminput"type="text" name="mail" placeholder="E-mail">
+            <input class ="forminput"type="password" name="pwd" placeholder="Password">
+            <input class ="forminput"type="password" name="pwd-repeat" placeholder="Repeat password">
+            <button class ="buttonform" type="submit" name="signup-submit">Signup</button>
+          </form>
+        </section>
+      </div>
+    </main>
 
 <?php
-    require "footer.php";
+  require "footer.php";
 ?>
